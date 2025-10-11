59c4b804f81db3c39aebab8e51c4d1851799d2d9
Kanesh Orachunlertmitri <kanesh132@gmail.com>
2025-10-11 20:22:59 +0700

    [feat] service get all term summary

--- src/modules/term-summary/term-summary.service.ts
+++ src/modules/term-summary/term-summary.service.ts
@@ -1,4 +1,14 @@
+import { DatabaseService } from '@/core/database/database.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TermSummaryService {}
+export class TermSummaryService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async getTermSummary(studentId: number) {
+    return this.databaseService.factTermSummary.findMany({
+      where: { studentId },
+      orderBy: [{ studyYear: 'desc' }, { studyTerm: 'desc' }],
+    });
+  }
+}
