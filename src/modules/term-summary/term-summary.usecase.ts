83671752ad52392c33f1f0a2ad3b9f5750d5e9bd
PrinTTer <prinn1101@gmail.com>
2025-10-11 18:03:42 +0700

    [feat] check is coop

--- src/modules/term-summary/term-summary.usecase.ts
+++ src/modules/term-summary/term-summary.usecase.ts
@@ -1,13 +1,9 @@
 import { DatabaseService } from '@/core/database/database.service';
 import { Injectable } from '@nestjs/common';
-import { StudentPlanService } from '../student-plan/student-plan.service';
 
 @Injectable()
 export class TermSummaryUseCase {
-  constructor(
-    private readonly databaseService: DatabaseService,
-    private readonly studentPlanService: StudentPlanService
-  ) {}
+  constructor(private readonly databaseService: DatabaseService) {}
 
   async checkFollowPlan(studentId: number, year: number, term: number) {
     const countPlanNotPass = await this.databaseService.factStudentPlan.count({
@@ -32,5 +28,27 @@ export class TermSummaryUseCase {
       },
     });
     return !countPlanNotPass;
+  }
+
+  async checkIsEligibleForCoop(
+    studentId: number,
+    coursePlanId: number,
+    creditAll: number,
+    year: number,
+    term: number
+  ) {
+    const coursePlan = await this.databaseService.coursePlan.findUnique({
+      where: { coursePlanId },
+    });
+
+    const creditIntern = coursePlan?.creditIntern ?? 0;
+    const canGoCoop = creditAll >= creditIntern;
+    if (!canGoCoop) return false;
+
+    const isFollowPlan = await this.checkFollowPlan(studentId, year, term);
+    if (isFollowPlan) {
+      return true;
+    }
+    return false;
   }
 }
