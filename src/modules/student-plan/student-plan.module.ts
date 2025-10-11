c80ac6e80f5685c5faf3b94f4d0a250df18330e2
Kanesh Orachunlertmitri <kanesh132@gmail.com>
2025-10-11 16:50:16 +0700

    [feat] service update student plan

--- src/modules/student-plan/student-plan.module.ts
+++ src/modules/student-plan/student-plan.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentPlanService } from './student-plan.service';
 import { StudentPlanUsecase } from './student-plan.usecase';
-import { SubjectCourseModule } from '../subject-course/subject-course.module';
+import { SubjectCourseModule } from '@/modules/subject-course/subject-course.module';
+import { RegisterModule } from '@/modules/register/register.module';
 
 @Module({
-  imports: [SubjectCourseModule],
+  imports: [SubjectCourseModule, RegisterModule],
   providers: [StudentPlanService, StudentPlanUsecase],
   exports: [StudentPlanService, StudentPlanUsecase],
 })
--- src/modules/student-plan/student-plan.usecase.ts
+++ src/modules/student-plan/student-plan.usecase.ts
@@ -4,14 +4,18 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
-import { SubjectCourseService } from '../subject-course/subject-course.service';
-import { Prisma } from '@prisma/client';
+import { FactRegister, Prisma } from '@prisma/client';
+import { SubjectCourseService } from '@/modules/subject-course/subject-course.service';
+import { RegisterService } from '@/modules/register/register.service';
+import { StudentPlanService } from './student-plan.service';
 
 @Injectable()
 export class StudentPlanUsecase {
   constructor(
     private readonly databaseService: DatabaseService,
-    private readonly subjectCourseService: SubjectCourseService
+    private readonly studentPlanService: StudentPlanService,
+    private readonly subjectCourseService: SubjectCourseService,
+    private readonly registerService: RegisterService
   ) {}
 
   async createStudentPlan(studentId: number) {
@@ -47,10 +51,76 @@ export class StudentPlanUsecase {
               ({
                 studentId,
                 subjectCourseId: subjectCourse.subjectCourseId,
+                isRequire: true, // todo: recheck this field when subjectCourse schema change
               }) as Prisma.FactStudentPlanCreateManyInput
           ),
         }),
       ]);
+      return true;
+    } catch (_) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async updateStudentPlan(studentId: number) {
+    try {
+      const [studentPlan, registers] = await Promise.all([
+        this.studentPlanService.getAllStudentPlan(studentId),
+        this.registerService.getAllRegisterByStudentId(studentId),
+      ]);
+
+      // Group registers by subjectCourseId
+      // previous term -> current term
+      const groupRegisterBySubjectCourseId = registers.reduce(
+        (map, register) => {
+          if (!register.subjectCourseId) return map;
+
+          const list = map.get(register.subjectCourseId) ?? [];
+          list.push(register);
+          map.set(register.subjectCourseId, list);
+
+          return map;
+        },
+        new Map<number, FactRegister[]>()
+      );
+
+      // only update student plan that are already registered
+      const filteredStudentPlan = studentPlan.filter(plan =>
+        groupRegisterBySubjectCourseId.has(plan.subjectCourseId)
+      );
+
+      await this.databaseService.$transaction(
+        filteredStudentPlan.map(plan => {
+          const registers =
+            groupRegisterBySubjectCourseId.get(plan.subjectCourseId) ?? [];
+
+          const latestRegister = registers[0];
+          const isCurrentTermPass = (latestRegister.gradeNumber ?? 0) > 0;
+
+          const note = registers
+            .reverse()
+            .map(register => register.gradeCharacter)
+            .join(',');
+
+          return this.databaseService.factStudentPlan.update({
+            where: {
+              studentId,
+              factStudentPlanId: plan.factStudentPlanId,
+            },
+            data: {
+              ...(isCurrentTermPass && {
+                passYear: latestRegister.studyYearInRegis,
+                passTerm: latestRegister.studyTermInRegis,
+              }),
+              isPass: isCurrentTermPass,
+              stdGrade: latestRegister.gradeNumber,
+              gradeDetails: note,
+            },
+          });
+        })
+      );
+
+      return true;
     } catch (_) {
       throw new InternalServerErrorException();
     }
